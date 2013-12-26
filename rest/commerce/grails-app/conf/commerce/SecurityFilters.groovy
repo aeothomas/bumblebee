c95c3f62b55342a8311689d035017ef4e8a9c8d6
Abidemi O-Thomas <aeothomas@gmail.com>
2013-12-26 14:06:48 +0000

    update security filter logic

--- rest/commerce/grails-app/conf/commerce/SecurityFilters.groovy
+++ rest/commerce/grails-app/conf/commerce/SecurityFilters.groovy
@@ -14,7 +14,18 @@ class SecurityFilters {
             }
         }*/
 
-        secure(controller:"product", action:"edit") {
+        restAuth(uri: '/merchants/*') {
+            before = {
+                //extract basic auth information so that it can be used for authentication in secure filter
+                if (!session.user && !actionName.equals('login')) {
+                    redirect(action: 'login')
+                    return false
+                }
+            }
+        }
+
+        //secure(controller:"product", action:"edit") {
+        secure(uri: '/merchants/*') {
             before = {
                 accessControl {
                     true
